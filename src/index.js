a846e113c32cab449b87d169519e8bdd09c6658b
ielkanyuk <ielkanyuk@gmail.com>
2020-12-04 12:04:27 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import {Subject} from 'rxjs'
+import {scan, startWith, shareReplay} from 'rxjs/operators'
+
+const initialState = {
+  counter: 0
+}
+
+const pre = document.querySelector('pre')
+
+const handlers = {
+  INCREMENT: state => ({...state, counter: state.counter + 1}),
+  DECREMENT: state => ({...state, counter: state.counter - 1}),
+  DEFAULT: state => state
+}
+
+function reducer(state = initialState, action) {
+  const handler = handlers[action.type] || handlers.DEFAULT
+  return handler(state, action)
+}
+
+function createStore(rootReducer) {
+  const subj$ = new Subject()
+
+  const store$ = subj$.pipe(
+    startWith({type: '__INIT__'}),
+    scan(rootReducer, undefined),
+    shareReplay(1)
+  )
+
+  store$.dispatch = action => subj$.next(action)
+
+  return store$
+}
+
+const store$ = createStore(reducer)
+
+store$.subscribe(state => {
+  pre.innerHTML = JSON.stringify(state, null, 2)
+})
+
+document.getElementById('increment').addEventListener('click', () => {
+  store$.dispatch({type: 'INCREMENT'})
+})
+
+document.getElementById('decrement').addEventListener('click', () => {
+  store$.dispatch({type: 'DECREMENT'})
+})
