f8da332a9c58d1cf14103fdb9d36c8b2a347d829
ielkanyuk <ielkanyuk@gmail.com>
2020-12-07 15:57:25 +0500

    Add shuffle

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import {Subject} from 'rxjs'
+import {Subject, fromEvent} from 'rxjs'
 import {scan, startWith, shareReplay} from 'rxjs/operators'
-import Table from './static_table.png'
 
 const initialState = {
-  counter: 0
+  counter: 0,
+  lastCard: 'none'
 }
 
 const pre = document.querySelector('pre')
+const lastCard = document.getElementById('lastCard')
 
 const handlers = {
-  Card_2: state => ({...state, counter: state.counter + 1}),
-  Card_3: state => ({...state, counter: state.counter + 2}),
-  Card_4: state => ({...state, counter: state.counter + 2}),
-  Card_5: state => ({...state, counter: state.counter + 3}),
-  Card_6: state => ({...state, counter: state.counter + 2}),
-  Card_7: state => ({...state, counter: state.counter + 1}),
-  Card_8: state => ({...state, counter: state.counter}),
-  Card_9: state => ({...state, counter: state.counter - 1}),
-  Card_10: state => ({...state, counter: state.counter - 2}),
+  Card_2: state => ({...state, counter: state.counter + 1, lastCard: '2'}),
+  Card_3: state => ({...state, counter: state.counter + 2, lastCard: '3'}),
+  Card_4: state => ({...state, counter: state.counter + 2, lastCard: '4'}),
+  Card_5: state => ({...state, counter: state.counter + 3, lastCard: '5'}),
+  Card_6: state => ({...state, counter: state.counter + 2, lastCard: '6'}),
+  Card_7: state => ({...state, counter: state.counter + 1, lastCard: '7'}),
+  Card_8: state => ({...state, counter: state.counter, lastCard: '8'}),
+  Card_9: state => ({...state, counter: state.counter - 1, lastCard: '9'}),
+  Card_10: state => ({...state, counter: state.counter - 2, lastCard: '10/A'}),
+  SHUFFLE: state => ({...state, counter: 0, lastCard: 'none'}),
   DEFAULT: state => state
 }
 
@@ -43,7 +45,7 @@ function createStore(rootReducer) {
 const store$ = createStore(reducer)
 
 store$.subscribe(state => {
-  pre.innerHTML = (state.counter / 6).toFixed(2);
+  pre.innerHTML = 'Count: ' + state.counter / 2 + ' | True count: ' + (state.counter / 6).toFixed(2) + ' | Last card: ' + state.lastCard;
 })
 
 document.getElementById('c_2').addEventListener('click', () => {
@@ -81,3 +83,46 @@ document.getElementById('c_9').addEventListener('click', () => {
 document.getElementById('c_10').addEventListener('click', () => {
   store$.dispatch({type: 'Card_10'})
 })
+
+document.getElementById('shuffle_btn').addEventListener('click', () => {
+  store$.dispatch({type: 'SHUFFLE'})
+})
+
+const keydown$ = fromEvent(document, 'keyup');
+keydown$.subscribe(($event) => {
+        switch($event.code) {
+          case 'Digit2':
+            store$.dispatch({type: 'Card_2'})
+            break;
+          case 'Digit3':
+            store$.dispatch({type: 'Card_3'})
+            break;
+          case 'Digit4':
+            store$.dispatch({type: 'Card_4'})
+            break;
+          case 'Digit5':
+            store$.dispatch({type: 'Card_5'})
+            break;
+          case 'Digit6':
+            store$.dispatch({type: 'Card_6'})
+            break;
+          case 'Digit7':
+            store$.dispatch({type: 'Card_7'})
+            break;
+          case 'Digit8':
+            store$.dispatch({type: 'Card_8'})
+            break;
+          case 'Digit9':
+            store$.dispatch({type: 'Card_9'})
+            break;
+          case 'Digit0':
+            store$.dispatch({type: 'Card_10'})
+            break;
+          case 'KeyR':
+            store$.dispatch({type: 'SHUFFLE'})
+            break;
+          default:
+            console.log($event);
+            break;
+        }
+    });
